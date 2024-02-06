6332cdd7bfde9af7c06c8f5e82d49cc457cdc95f
borhan98 <borhanuddindns420@gmail.com>
2024-02-06 13:50:05 +0600

    Slider modified

--- js/main.js
+++ js/main.js
@@ -2,10 +2,20 @@
 AOS.init({disable: 'mobile'});
 
 // Slick slider
+$('.clients').slick({
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 1500,
+  variableWidth: true,
+  arrows: false
+});
+
+// Slick slider
 $('.slide-wrapper').slick({
   slidesToShow: 2,
   slidesToScroll: 1,
-  autoplay: false,
+  autoplay: true,
   autoplaySpeed: 1500,
   variableWidth: true,
   prevArrow: $('.prev-arrow'),
